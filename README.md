77dfeec4d6a135008eb04a4ada6996c53d23d236
elva <jia_shen@126.com>
2025-06-11 15:25:50 +0800

    feat: added server side logic to login with Github

--- README.md
+++ README.md
@@ -1,54 +1,12 @@
-# React + TypeScript + Vite
+This is a sample demo of GitHub Login With React (GitHub APIs, GitHub OAuth 2.0 Authentication), following the tutorial https://www.youtube.com/watch?v=rRn2EisxPl4
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+The details steps are:
 
-Currently, two official plugins are available:
+* Initialize GitHub OAuth 2.0 Application
+* Creating initial login logic
+* Process code parameter from URL
+* Create proxy server / call GitHub APIs
+* Create access token logic for frontend
+* Get user data with access token
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
-
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend updating the configuration to enable type-aware lint rules:
-
-```js
-export default tseslint.config({
-  extends: [
-    // Remove ...tseslint.configs.recommended and replace with this
-    ...tseslint.configs.recommendedTypeChecked,
-    // Alternatively, use this for stricter rules
-    ...tseslint.configs.strictTypeChecked,
-    // Optionally, add this for stylistic rules
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
-  languageOptions: {
-    // other options...
-    parserOptions: {
-      project: ['./tsconfig.node.json', './tsconfig.app.json'],
-      tsconfigRootDir: import.meta.dirname,
-    },
-  },
-})
-```
-
-You can also install [eslint-plugin-react-x](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-x) and [eslint-plugin-react-dom](https://github.com/Rel1cx/eslint-react/tree/main/packages/plugins/eslint-plugin-react-dom) for React-specific lint rules:
-
-```js
-// eslint.config.js
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
-
-export default tseslint.config({
-  plugins: {
-    // Add the react-x and react-dom plugins
-    'react-x': reactX,
-    'react-dom': reactDom,
-  },
-  rules: {
-    // other rules...
-    // Enable its recommended typescript rules
-    ...reactX.configs['recommended-typescript'].rules,
-    ...reactDom.configs.recommended.rules,
-  },
-})
-```
+For Github authentication, you need to create the OAuh Apps in the Github => Settings => Developer Settings => OAuth, fill in the homepage url and callback url, for localhost, it could be http://localhost:5173/. When deployed to production, you need to update the application callback url.
--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import type { GithubUser } from './types/types';
+import './index.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [rerender, setRerender] = useState(false);
+  const [userData, setUserData] = useState<GithubUser>();
+
+  useEffect(() => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const codeParam = urlParams.get('code');
+
+    if (codeParam && !localStorage.getItem('accessToken')) {
+      const getAccessToken = async () => {
+        try {
+          const response = await fetch(`http://localhost:4000/getAccessToken?code=${codeParam}`);
+          const data = await response.json();
+          if (data.access_token) {
+            localStorage.setItem('accessToken', data.access_token);
+            setRerender((prev) => !prev);
+          }
+        } catch (error) {
+          console.error('Error fetching access token:', error);
+        }
+      };
+      getAccessToken();
+    }
+
+    if (localStorage.getItem('accessToken')) {
+      getUserData();
+    }
+  }, [rerender]);
+
+  const getUserData = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/getUserData', {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer ' + localStorage.getItem('accessToken'),
+        },
+      });
+      const data = await response.json();
+      console.log('userData', data)
+      setUserData(data);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  const handleGithubLogin = () => {
+    window.location.assign(
+      `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}`
+    );
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {localStorage.getItem('accessToken') ? (
+        <div className="absolute top-10 right-6 flex items-center gap-1">
+          <img src={userData?.avatar_url} alt="Avatar" className='w-6 h-6 rounded-full' />
+          <h1 className='text-sm font-medium'>{userData?.login || 'User'}</h1>
+          <div
+            onClick={() => {
+              localStorage.removeItem('accessToken');
+              setRerender((prev) => !prev);
+            }}
+            className='underline text-blue-500 text-sm ml-2 cursor-pointer font-bold'
+          >
+            Log out
+          </div>
+        </div>
+      ) : (
+        <div className="text-red-400" onClick={handleGithubLogin}>
+          Login with github
+        </div>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
